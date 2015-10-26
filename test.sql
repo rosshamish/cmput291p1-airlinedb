546695f23bc7a9ea637c1ea77c7e57d467473f50
Bradshawz <abradsha@ualberta.ca>
2015-10-26 16:36:41 -0600

    Added some sql inserts

---
+++ test.sql
@@ -0,0 +1,8 @@
+INSERT INTO users VALUES('a@b', 'ab', null);
+INSERT INTO airline_agents VALUES('a@b', 'ab');
+INSERT INTO fares VALUES('fa', 'fa');
+INSERT INTO passengers VALUES('a@b', 'ab', 'canada');
+INSERT INTO tickets VALUES(1, 'ab', 'a@b', 20);
+INSERT INTO tickets VALUES(2, 'ab', 'a@b', 30);
+INSERT INTO bookings VALUES(1, 'WJ1406', 'fa', '22-DEC-15', 2);
+INSERT INTO bookings VALUES(2, 'WJ1406', 'fa', '22-DEC-15', 3);
